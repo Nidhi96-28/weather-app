189d4b641c1a22a266d93beaa79647c264447ce4
Nidhi Singh <nidhisingh@Nidhi-MacBook-Air.local>
2026-03-29 02:15:41 +0530

    azure changes

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { WeatherResponse } from "./weather.model";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,10 @@ export class WeatherService {
   constructor() { }
 
   private http = inject(HttpClient);
-  private apiKey = '0027433bd0a6350ffda9d1b75b193b04'; // Replace with your actual API key
-  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  
+   baseUrl = environment.baseUrl;
   getWeather(city: string): Observable<WeatherResponse> {
-    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    const url = `${this.baseUrl}?city=${city}`;
     return this.http.get<WeatherResponse>(url);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://weather-fn-nidhi123-gseecbh9ewerakea.southindia-01.azurewebsites.net/api/getWeather',
+};
