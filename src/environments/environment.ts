b2fed9ac4483da8ec23b8083923ec92215dbf524
Nidhi Singh <nidhisingh@Nidhi-MacBook-Air.local>
2026-03-30 00:34:19 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://weather-fn-nidhi123-gseecbh9ewerakea.southindia-01.azurewebsites.net/api/getWeather',
+  //baseUrl: 'https://weather-fn-nidhi123-gseecbh9ewerakea.southindia-01.azurewebsites.net/api/getWeather',
+  baseUrl: 'http://localhost:7071/api/getWeather'
 };
